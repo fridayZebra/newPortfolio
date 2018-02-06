870413754f3147ac62d231f13a0f083b49bf88b4
Firoz Kamdar <firozkamdar@gmail.com>
2018-02-06 01:04:37 -0800

    navbar shows on scrolling past main image; needs to be sized and spaced;  footer added- needs more work

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 'use strict';
+
+$(document).scroll(() => {
+  if( $(this).scrollTop() > 1000) {
+    $('.navbar').removeClass('hidden');
+    $('.navbar').addClass('js-navbar-onScroll');
+  } else {
+    $('.navbar').addClass('hidden');
+    $('.navbar').removeClass('js-navbar-onScroll');
+  }
+});
+
