fb0c2a0f450db4516158600a9a718b0ce03da112
firoz <27827437+hiroze@users.noreply.github.com>
2018-02-23 21:04:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// === REMINDER ===> fix code so navbar display isn't pixel based
 $(document).scroll(() => {
   if( $(this).scrollTop() > 1000) {
     $('.navbar').removeClass('hidden');
